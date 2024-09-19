847cc4b5bc9ed12a526711b0b3d76edbf48d4ecd
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2024-09-19 13:24:05 -0400

    Remove subshell from script
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/cli-builder.sh
+++ build/cli-builder.sh
@@ -11,27 +11,25 @@ BUILD_DIR=${BUILD_DIR:-${SCRIPT_DIR}}
 while IFS=, read -r git_url build_cmd build_dir; do
   git_repo=${git_url##*/}
 
-  (
-    cd "${REMOTE_SOURCES_DIR}/${git_repo}/${REMOTE_SOURCES_SUBDIR}"
+  cd "${REMOTE_SOURCES_DIR}/${git_repo}/${REMOTE_SOURCES_SUBDIR}"
 
-    # Source relevant cachito.env for downstream builds
-    cachito_path="${REMOTE_SOURCES_DIR}/${git_repo}"/cachito.env
-    if [[ -f "${cachito_path}" ]]; then
-      source "${cachito_path}"
-    fi
+  # Source relevant cachito.env for downstream builds
+  cachito_path="${REMOTE_SOURCES_DIR}/${git_repo}"/cachito.env
+  if [[ -f "${cachito_path}" ]]; then
+    source "${cachito_path}"
+  fi
 
-    # Set branch using downstream UPSTREAM_BRANCH variable
-    # ref: https://cpaas.pages.redhat.com/documentation/users/midstream/generating_files_from_templates.html#_environment_variables
-    repo_upper=$(echo ${git_repo} | tr '[:lower:]' '[:upper:]')
-    branch_var=CI_${repo_upper//-/_}_UPSTREAM_BRANCH
-    if [[ -z "$(git branch --show-current)" ]] && [[ -n "${!branch_var}" ]]; then
-      git checkout -b ${!branch_var}
-    fi
+  # Set branch using downstream CI_UPSTREAM_BRANCH variable
+  # ref: https://cpaas.pages.redhat.com/documentation/users/midstream/generating_files_from_templates.html#_environment_variables
+  if [[ -z "$(git branch --show-current)" ]] && [[ -n "${CI_UPSTREAM_BRANCH}" ]]; then
+    git checkout -b ${CI_UPSTREAM_BRANCH}
+  fi
 
-    echo "* Building binaries from ${git_url}"
-    echo "* Executing build command: ${build_cmd}"
-    ${build_cmd}
-    echo "* Moving binaries from repo directory <repo>/${build_dir}/ to: ./${BUILD_DIR}/"
-    mv ${build_dir}/* ${SCRIPT_DIR}/../${BUILD_DIR}
-  )
+  echo "* Building binaries from ${git_url}"
+  echo "* Executing build command: ${build_cmd}"
+  ${build_cmd}
+  echo "* Moving binaries from repo directory <repo>/${build_dir}/ to: ./${BUILD_DIR}/"
+  mv ${build_dir}/* ${SCRIPT_DIR}/../${BUILD_DIR}
+
+  cd - 1>/dev/null
 done <${SCRIPT_DIR}/cli_map.csv
